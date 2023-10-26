53e2e9813576f2e89ea7e5021fdf1611152f56b7
musamM921 <musaartlove@gmail.com>
2023-10-26 17:39:41 +0400

    push2

--- iphoneadves/index.js
+++ iphoneadves/index.js
@@ -8,10 +8,22 @@ const end = section.querySelector('h1');
 //scrollmagic
 const controller = new ScrollMagic.Controller();
 
+//scenes
 const scene = new ScrollMagic.Scene({
     duration: 1000,
     triggerElement: intro, 
     triggerHook: 0
 })
 .addIndicators()
+.setPin(intro)
 .addTo(controller);
+
+//video animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollpos;
+    console.log(e)
+})
